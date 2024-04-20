d6af92450634acbe5e250afe8a7826713563db7d
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-04-20 12:30:13 +0600

    daisy and React icons install

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import Banner from "../../components/banner/Banner";
 
 
@@ -6,6 +7,13 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <div>
+                <ul className="flex justify-center gap-4 mt-12">
+                    <li><NavLink className='btn'>Women</NavLink></li>
+                    <li><NavLink className='btn'>Man</NavLink></li>
+                    <li><NavLink className='btn'>Kids</NavLink></li>
+                </ul>
+            </div>
         </div>
     );
 };
